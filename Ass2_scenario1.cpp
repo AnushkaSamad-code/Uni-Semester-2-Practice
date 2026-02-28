2178dcff673841f215bb8843965a5f719b24df03
Anushka Samad <anushkasamad276@gmail.com>
2026-02-28 15:00:08 +0500

    scenario1
    suppose that we argently need to add a bus form,that records the following information:
    color,engin size,transmission type,
    NUMBER OF PASSENGERS

---
+++ Ass2_scenario1.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Vehicle {
+public:
+    string color;
+    string engineSize;
+    string transmissionType;
+
+    void showVehicleInfo() {
+        cout << "Color: " << color << endl;
+        cout << "Engine: " << engineSize << endl;
+        cout << "Transmission: " << transmissionType << endl;
+    }
+};
+
+class Bus : public Vehicle {
+public:
+    int numberOfPassengers; 
+
+    void showBusInfo() {
+        showVehicleInfo(); 
+        cout << "Passengers Capacity: " << numberOfPassengers << endl;
+    }
+};
+
+int main() {
+    Bus mySchoolBus;
+
+    mySchoolBus.color = "Yellow";
+    mySchoolBus.engineSize = "V8 Heavy Duty";
+    mySchoolBus.transmissionType = "Automatic";
+    
+    mySchoolBus.numberOfPassengers = 50;
+
+    cout << "--- Meri Bus Ki Details ---" << endl;
+    mySchoolBus.showBusInfo();
+
+    return 0;
+}
